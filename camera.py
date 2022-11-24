3d86cc358cacf234f746f738122a19e1b0e0dd24
Krzysztof Pęczek <peczek.kr@gmail.com>
2022-11-24 23:16:08 +0100

    Added taking a still during backup recording as a feature

--- camera.py
+++ camera.py
@@ -1,16 +1,41 @@
+from typing import Tuple
+
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encode
+from picamera2.encoders import H264Encoder, Quality
 
 
-class Camera:
-    def __init__(self):
-        self.m_camera = Picamera2()
+class Camera():
+    def __init__(self, main_resolution, backup_resolution):
+        self._camera = Picamera2()
 
-    def startRecording(self, filepath):
-        video_config = self.m_camera.create_video_configuration()
-        self.m_camera.configure(video_config)
-        encoder = H264Encoder(10000000)
-        self.m_camera.start_recording(encoder, filepath)
+        self.verifyResolution(main_resolution)
+        self.verifyResolution(backup_resolution)
 
-    def stopRecording(self):
-        self.m_camera.stop_recording()
+        self._configuration = self.__createConfig(
+            main_resolution, backup_resolution)
+        self._camera.configure(self._configuration)
+        self._encoder = H264Encoder()
+
+    def startBackupRecording(self, filepath):
+        self._camera.start_recording(self._encoder, filepath, Quality.MEDIUM)
+
+    def stop(self):
+        self._camera.stop_recording()
+
+    def pollCurrent(self):
+        request = self._camera.capture_request()
+        result = request.make_array("main")
+        request.release()
+        return result
+
+    def __createConfig(self, main_resolution, backup_resolution):
+        # This configures the camera so that the backup stream with backup_resolution can be recorded,
+        # but at any point a still image of resolution main_resolution can be taken.
+        return self._camera.create_video_configuration(
+            {"size": main_resolution}, {"size": backup_resolution}, encode="lores")
+
+    def __verifyResolution(self, resolution):
+        if resolution[0] > self._camera.sensor_resolution[0] or resolution[1] > self._camera.sensor_resolution[1]:
+            raise ValueError("Given resolution is too large!")
+        elif resolution[0] < 64 or resolution[1] < 64:
+            raise ValueError("Given resolution is too small!")
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 
 def main():
-    camera = Camera()
+    camera = Camera(main_resolution=(1920, 1080), backup_resolution=(640, 480))
     camera.startRecording("hello_world.h264")
     time.sleep(10)
     camera.stopRecording()
